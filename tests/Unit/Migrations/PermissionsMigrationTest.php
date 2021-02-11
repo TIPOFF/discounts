cc660289d93002d165acc5ad348bbc3c15ee6fe3
Patrick Breen <patrick.breen@quartzy.com>
2021-02-11 10:35:57 -0500

    Require authorization

--- tests/Unit/Migrations/PermissionsMigrationTest.php
+++ tests/Unit/Migrations/PermissionsMigrationTest.php
@@ -6,19 +6,11 @@ namespace Tipoff\Discounts\Tests\Unit\Migrations;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Support\Facades\Schema;
-use Spatie\Permission\PermissionServiceProvider;
 use Tipoff\Discounts\Tests\TestCase;
 
 class PermissionsMigrationTest extends TestCase
 {
     use DatabaseTransactions;
-
-    protected function getPackageProviders($app)
-    {
-        return array_merge(parent::getPackageProviders($app), [
-            PermissionServiceProvider::class,
-        ]);
-    }
 
     /** @test */
     public function permissions_seeded()
