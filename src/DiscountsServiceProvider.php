2fcbd052fefca1acfa1d33532d3479fbf6d76551
Drew Roberts <github@drewroberts.com>
2021-02-14 07:26:02 -0500

    Remove extra spaces

--- src/DiscountsServiceProvider.php
+++ src/DiscountsServiceProvider.php
@@ -15,11 +15,11 @@ class DiscountsServiceProvider extends TipoffServiceProvider
     public function configureTipoffPackage(TipoffPackage $package): void
     {
         /**
-         * Tipoff specific methods should precede base methods to avoid Psalm error	
+         * Tipoff specific methods should precede base methods to avoid Psalm error
          */
         $package
             ->hasModelInterfaces([
-                DiscountInterface::class => Discount::class,	
+                DiscountInterface::class => Discount::class,
             ])
             ->hasPolicies([
                 Discount::class => DiscountPolicy::class,
